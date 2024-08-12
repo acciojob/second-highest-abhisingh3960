91f1db9f37ce686a812917e1886dee7dbf840f5f
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-08-12 12:53:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-//your JS code here. If required.
-let largest=-Infinity;
-let secLar=-Infinity;
-for(let i=0 ; i< arr.length ; i++){
-    if(arr[i]>largest){
-         secLar = largest;  
-         largest = arr[i];
-    }
-    else if(arr[i]>secLar && arr[i] !==largest ){
-        secLar = arr[i]; 
-    }
-    
+let arr = [];
+let n = prompt("Enter the number of elements:");
+
+for (let i = 0; i < n; i++) {
+    arr.push(parseInt(prompt(`Enter element ${i + 1}:`), 10));
 }
-console.log(secLar);
+
+let largest = -Infinity;
+let secLar = -Infinity;
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+        secLar = largest;
+        largest = arr[i];
+    } else if (arr[i] > secLar && arr[i] !== largest) {
+        secLar = arr[i];
+    }
+}
+
+alert(secLar);
