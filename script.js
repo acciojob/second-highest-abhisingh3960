f520a82d4ac33f51322bb522942bfc42a0918e25
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-08-12 12:50:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let largest=-Infinity;
+let secLar=-Infinity;
+for(let i=0 ; i< arr.length ; i++){
+    if(arr[i]>largest){
+         secLar = largest;  
+         largest = arr[i];
+    }
+    else if(arr[i]>secLar && arr[i] !==largest ){
+        secLar = arr[i]; 
+    }
+    
+}
+console.log(secLar);
