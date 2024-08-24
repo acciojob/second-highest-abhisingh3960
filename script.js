041353d114be238990300331f35467dfca7b83fa
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-08-24 17:48:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function secondHighest(arr) {
 			// If the array is empty, has only 1 element, or all elements are the same, return -Infinity
 			if (arr.length < 2) {
 				return -Infinity;
-				
 			}
 			
 			// Remove duplicate elements by converting the array to a Set and back to an array
@@ -19,3 +18,14 @@ function secondHighest(arr) {
 			// Return the second highest element
 			return uniqueArr[uniqueArr.length - 2];
 		}
+
+function Main() {
+    var n = prompt("Enter the number of elements");
+    var arr = [];
+    for (var i = 0; i < n; i++) {
+        arr[i] = prompt("Enter element " + (i + 1));
+    }
+    alert(secondHighest(arr));
+}
+
+Main();
