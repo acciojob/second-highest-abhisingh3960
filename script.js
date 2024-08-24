b7e78a6785999b509b2e8cd2f76418ff648f736d
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-08-24 17:41:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
 function secondHighest(arr) {
-    // Handle edge cases
-	
-    if (arr.length < 2) return -Infinity;
-    
-    let highest = -Infinity;
-    let secondHighest = -Infinity;
-    
-    for (let i = 0; i < arr.length; i++) {
-        let num = arr[i]; // Convert input to a number
-        if (num > highest) {
-            secondHighest = highest;
-            highest = num;
-        } else if (num > secondHighest && num < highest) {
-            secondHighest = num;
-        }
-    }
-
-
-	
-	
-    return secondHighest == -Infinity ? -Infinity : secondHighest;
-}
-
-function Main() {
-    var n = prompt("Enter the number of elements");
-    var arr = [];
-    for (var i = 0; i < n; i++) {
-        arr[i] = prompt("Enter element " + (i + 1));
-    }
-    alert(secondHighest(arr));
-}
-
-Main();
+			// If the array is empty, has only 1 element, or all elements are the same, return -Infinity
+			if (arr.length < 2) {
+				return -Infinity;
+			}
+			
+			// Remove duplicate elements by converting the array to a Set and back to an array
+			let uniqueArr = [...new Set(arr)];
+			
+			// If the array has less than 2 unique elements, return -Infinity
+			if (uniqueArr.length < 2) {
+				return -Infinity;
+			}
+			
+			// Sort the unique array in ascending order
+			uniqueArr.sort((a, b) => a - b);
+			
+			// Return the second highest element
+			return uniqueArr[uniqueArr.length - 2];
+		}
